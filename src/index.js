4590941601c210dfcbffdbcf60bb8a3312663c08
nwangasam <46712385+nwangasam@users.noreply.github.com>
2020-04-05 17:00:39 +0100

    Getting started with http in react js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App title="These are Relevant persons!" />,
+  <App />,
   document.getElementById('root')
 );
 
